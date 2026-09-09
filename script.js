2dc63be26d459c0623d23fc478a3a81a6fda0bff
MaheshGavande <gawandemahesh420@gmail.com>
2026-09-09 13:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+//your JS code here.
+
+
+let p=document.querySelector('p');
+let div =document.querySelector('div');
+
+let button  = document.querySelector('button');
+button.addEventListener('click' , ()=>{
+    div.id = "weather"
+   // p.innerText = "";
+   p.innerText = "Current weather in London : Clouds"; 
+})
